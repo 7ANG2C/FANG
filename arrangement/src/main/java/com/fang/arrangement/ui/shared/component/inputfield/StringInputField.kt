9fcc51637e9c2cd2f6c247dc3ec5ef37943657a0
FANG <shelly53553@gmail.com>
2024-10-06 18:49:24 +0800

    feat: Optimize blank check for StringInputField.

--- arrangement/src/main/java/com/fang/arrangement/ui/shared/component/inputfield/StringInputField.kt
+++ arrangement/src/main/java/com/fang/arrangement/ui/shared/component/inputfield/StringInputField.kt
@@ -1,10 +1,22 @@
 package com.fang.arrangement.ui.shared.component.inputfield
 
+import androidx.compose.foundation.text.input.TextFieldBuffer
 import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.delete
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
 import com.fang.cosmos.foundation.Action
+import com.fang.cosmos.foundation.indexOfFirstOrNull
+
+private const val EMPTY_STRING_HOLDER = "@%^&@%^&"
+private val textFieldBufferBlock: TextFieldBuffer.() -> Unit by lazy {
+    {
+        val index = toString().indexOfFirstOrNull { it != ' ' } ?: length
+        delete(0, index)
+    }
+}
 
 @Composable
 internal fun StringInputField(
@@ -14,19 +26,30 @@ internal fun StringInputField(
     imeAction: ImeAction = ImeAction.Unspecified,
     lines: Int? = null,
     onClear: Boolean = false,
-    onValueChange: Action<String>,
-) = BaseInputField(
-    modifier = modifier,
-    titleText = titleText,
-    text = text,
-    inputTransformation = {
-        if (toString() == " ") revertAllChanges()
-    },
-    imeAction = imeAction,
-    lineLimits =
+    onValueChange: Action<String?>,
+) {
+    val textFieldState = rememberTextFieldState(EMPTY_STRING_HOLDER)
+    if (textFieldState.text == EMPTY_STRING_HOLDER) {
+        textFieldState.edit {
+            replace(0, length, text.orEmpty())
+            textFieldBufferBlock(this)
+            onValueChange(toString())
+        }
+    }
+    BaseInputField(
+        modifier = modifier,
+        titleText = titleText,
+        textFieldState = textFieldState,
+        inputTransformation = {
+            textFieldBufferBlock(this)
+            onValueChange(toString())
+        },
+        imeAction = imeAction,
+        lineLimits =
         lines?.let {
             TextFieldLineLimits.MultiLine(maxHeightInLines = it)
         } ?: TextFieldLineLimits.Default,
-    onClear = onClear,
-    onValueChange = onValueChange,
-)
+        onClear = onClear,
+        onValueChange = onValueChange,
+    )
+}
