c685e62da26cf8f5a1d420da8bbc16aaca91b2fd
FANG <shelly53553@gmail.com>
2024-10-06 20:14:23 +0800

    1.3.0 (4)

--- arrangement/build.gradle.kts
+++ arrangement/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     resourcePrefix = "arr_"
     defaultConfig {
         applicationId = pkg
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.3.0"
         vectorDrawables.useSupportLibrary = true
         ndk { abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a")) }
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
