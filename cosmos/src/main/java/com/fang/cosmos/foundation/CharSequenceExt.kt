56bf89433e730b5ffafd9be263e89c2eca5f7902
FANG <shelly53553@gmail.com>
2024-10-06 20:08:56 +0800

    feat: Optimize non-digit check for NumberInputField.

--- cosmos/src/main/java/com/fang/cosmos/foundation/CharSequenceExt.kt
+++ cosmos/src/main/java/com/fang/cosmos/foundation/CharSequenceExt.kt
@@ -3,5 +3,5 @@ package com.fang.cosmos.foundation
 inline fun CharSequence.indexOfFirstOrNull(predicate: (Char) -> Boolean): Int? =
     indexOfFirst(predicate).takeIf { it >= 0 }
 
-inline fun CharSequence.indexOfLaseOrNull(predicate: (Char) -> Boolean): Int? =
+inline fun CharSequence.indexOfLastOrNull(predicate: (Char) -> Boolean): Int? =
     indexOfLast(predicate).takeIf { it >= 0 }
